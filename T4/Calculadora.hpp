af5248c687844e204a2f5f4e969c8e19fa210df3
Daniel dos Santos Pereira <danield1591998@gmail.com>
2019-09-30 12:57:20 -0300

    Precedencia de operadores implementada

--- T4/Calculadora.hpp
+++ T4/Calculadora.hpp
@@ -3,13 +3,30 @@
 #include <stack>
 #include <iostream>
 #include <cmath>
+#include <vector>
+
+struct Operador {
+    char op;
+    int weight;
+    bool operator<(Operador &o) {
+        return weight < o.weight;
+    }
+    bool operator>(Operador &o) {
+        return weight > o.weight;
+    }
+};
 
 template<typename T>
+
 struct Calculadora {
 
     // ver http://en.cppreference.com/w/cpp/container/stack
     std::stack<T> operandos;    // números, basicamente
     std::stack<char> operadores; // operadores: +, -, /, *
+    std::vector<char> shadowOperadores;
+    std::vector<Operador> orderedOperadores;
+    std::vector<T> shadowOperandos;
+
     bool hasSyntaxError = false;
     // podem incluir mais dados, se necessário
 
@@ -34,31 +51,128 @@ struct Calculadora {
     // se for um abre parenteses '(' tudo ok. 
     // se for um fecha parenteses ')', deve-se calcular tudo dentro.
     void operador(char op){
-        // verficar aqui o operador inserido
+        if(hasSyntaxError) return;//Caso tenha algum erro de sintaxe
+        
         if(op == ')'){
-            while(!operadores.empty() && operadores.top() != '(')
-                calcula();
+            resolveStack();
+        } else {
+            operadores.push(op);
+        }   
+    }
 
-            if(!operadores.empty()){
+    void insertionSort(std::vector<Operador> &array){
+        int cursor;
+        Operador key;
+        bool end;
+        for(int key_pos = 1; key_pos < array.size(); key_pos++){
+            key = array[key_pos];
+            cursor = key_pos - 1;
+            while(cursor >= 0 && array[cursor] > key){
+                array[cursor+1] = array[cursor];
+                cursor--;
+            }
+            array[cursor+1] = key;
+        }
+    }
+
+    void resolveStack(){
+        if(operadores.empty()) return;
+
+        shadowOperadores.clear();
+        orderedOperadores.clear();
+        shadowOperandos.clear();
+
+        int count = 0;
+        while(!operadores.empty()){//Copy Operadores
+            if(operadores.top() != '('){
+                count++;
+                shadowOperadores.push_back(operadores.top());
+                operadores.pop();//Remove the top element
+            } else {
                 operadores.pop();
+                break;
+            }
+        }
+
+        if(shadowOperadores.empty()) return;
+
+        if(count+1 > operandos.size()){
+            hasSyntaxError = true;
+            return;
+        }
+        
+        //O total de operadores e uma unidade menor que o numero de operandos 
+        for(int i = 0; i < count+1; i++){//Copy Operandos
+            shadowOperandos.push_back(operandos.top());
+            operandos.pop();
+        }
+
+        if(shadowOperandos.size() <= shadowOperadores.size()){
+            hasSyntaxError = true;
+            return;
+        }
+
+        for(auto &v: shadowOperadores){
+            orderedOperadores.push_back({v, operadorWeight(v)});
+        }
+        
+        insertionSort(orderedOperadores);
+
+        if(shadowOperadores.size() == 0){
+            if(shadowOperandos.size() == 1){
+                operandos.push(shadowOperandos.at(0));
             } else {
                 hasSyntaxError = true;
             }
-        } else {
-            operadores.push(op);
+            return;
         }
-        
+
+        for(int i = 0; i < orderedOperadores.size(); i++){
+            for(int pos = 0; shadowOperadores.size(); pos++){
+                if(shadowOperadores.at(pos) == orderedOperadores.at(i).op){
+                    //Send data to stack
+                    operadores.push(shadowOperadores.at(pos));
+                    eraseVectorCharPos(shadowOperadores, pos);
+
+                    operandos.push(shadowOperandos.at(pos+1));
+                    eraseVectorPos(shadowOperandos, pos+1);
+
+                    operandos.push(shadowOperandos.at(pos));
+                    eraseVectorPos(shadowOperandos, pos);
+                    
+                    //Calcula com o que tem na stack
+                    calcula();
+
+                    //Coloca no `shadowOperandos` pra continuar o processo e remove o resultado de `operandos`
+                    insertInPos(shadowOperandos, pos, operandos.top());
+                    if(i != orderedOperadores.size() - 1){//Se nao e a ultima operacao, entao remove da pilha operandos
+                        operandos.pop();
+                    }
+
+                    break; 
+                }
+            }
+        }
+    }
+
+    void eraseVectorPos(std::vector<T> &vect, int pos){
+        vect.erase(vect.begin() + pos);
+    }
+    void eraseVectorCharPos(std::vector<char> &vect, int pos){
+        vect.erase(vect.begin() + pos);
+    }
+
+    void insertInPos(std::vector<T> &vect, int pos, T val){
+        vect.insert(vect.begin() + pos, val); 
     }
 
     // finaliza o calculo, retorna false se erro detectado
     bool fim(void) {
-        while (!operadores.empty())
-        {
-            if(!calcula())
-                return false;
+        if(!hasSyntaxError){
+            resolveStack();
         }
         
-        return true && !hasSyntaxError;
+        return !hasSyntaxError;
     }
 
     //Retorna true se a operacao foi realizada com sucesso
@@ -110,5 +224,18 @@ struct Calculadora {
     // esse resultado sera parcial se ainda nao foi chamado calc_fim().
     T resultado(void) {
         return operandos.top();
+    }
+
+    int operadorWeight(char op){
+        if(op == '^'){
+            return 1;
+        } else if(op == '*' || op == '/'){
+            return 2;
+        } else if(op == '+' || op == '-'){
+            return 3;
+        } else {
+            hasSyntaxError = true;
+            return 4;
+        }
     }
 };
--- T4/main.cpp
+++ T4/main.cpp
@@ -6,9 +6,9 @@ void testaFloat(void)
     Calculadora<float> calc;
     float x;
 
-    calc.operando(13.1);
-    calc.operador('+');
-    calc.operando(17.6);
+    calc.operando(0.5);
+    calc.operador('^');
+    calc.operando(2);
     
     if(calc.fim() == false)
         std::cout << "ERRO: formula errada!" << std::endl;
@@ -23,30 +23,38 @@ void testaInt(void)
     Calculadora<int> calc;
     int x;
 
-    // calc.operador('(');
     calc.operador('(');
-    calc.operando(2);
+    calc.operador('(');
+    calc.operando(3);
     calc.operador(')');
+    calc.operador('*');
+    calc.operando(4);
     calc.operador('+');
-    calc.operando(1);
+    calc.operando(5);
     calc.operador('^');
     calc.operando(2);
-    // calc.operador(')');
-    // calc.operador('*');
-    // calc.operando(2);
+    calc.operador('-');
+    calc.operando(1);
+    calc.operador(')');
+    //RIGHT 12+10 = 22
+    //     
+    //     5        
+    // *   2       10
+    // +   3    +   3
+    // *   4    *   4
 
 
     if(calc.fim() == false)
         std::cout << "ERRO: formula errada!" << std::endl;
     
     x = calc.resultado();
-    std::cout << "((2+13)+2)*2 = " << x << std::endl;
+    std::cout << "((3)*4+5^2-1) = " << x << std::endl;
     calc.destroi();
 }
 
 int main(int argc, char** argv)
 {
-    testaFloat();
+    // testaFloat();
     testaInt();
     return 0;
 }

140509e5dce39d82dedbf6b9b9e54584a8dbec5f
Daniel dos Santos Pereira <danield1591998@gmail.com>
2019-10-27 19:33:04 -0300

    Functions to deal with formulas

--- T5/util.hpp
+++ T5/util.hpp
@@ -3,6 +3,11 @@
 #include "structs.hpp"
 #include <string>
 #include <ctime>
+#include <vector>
 
 std::string position2address(Position pos);
 double get_elapsed_time(std::clock_t begin);
+std::vector<double> extractNumbers(std::string target);
+std::vector<char> extractSymbols(std::string target);
+std::vector<std::string> extractReferences(std::string target);
+std::string extractSequenceSymbolNumber(std::string target);
